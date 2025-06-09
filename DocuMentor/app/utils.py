62d0f2924b84993383baf57fb7779fc80a5d3699
ApoorvaPa <apoorvaparashar09@gmail.com>
2025-06-09 18:07:23 +0530

    first incoming commit from endeavourOS

--- DocuMentor/app/utils.py
+++ DocuMentor/app/utils.py
@@ -8,4 +8,5 @@ def chunk_text(text, chunk_size=500, overlap=50):
     for i in range(0, len(words), chunk_size - overlap):
         chunk = " ".join(words[i:i + chunk_size])
         chunks.append(chunk)
+        #Checking chunking from linux
     return chunks
